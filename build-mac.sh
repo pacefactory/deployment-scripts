eef63e08a57d7a6182a93941f53a7d2bfd524e11
MattGrouchy <mattgrouchy@yahoo.com>
2025-06-23 16:33:22 -0230

    Add comments as reviewing previous changes

--- build-mac.sh
+++ build-mac.sh
@@ -8,12 +8,21 @@ if [[ "$(docker images -q pf-build 2> /dev/null)" == "" ]]; then
     docker build -f scripts/Dockerfile.build -t pf-build .
 fi
 
-# Run the build in container with Docker socket mounted 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo ""
+echo -e "${RED}Attention:${NC} The docker daemon is not mounted to the container. So it will say that it cannot find the daemon, \
+but it is not needed for running docker compose config"
+echo ""
+
+# Run the build in container with Docker
 
 # The --rm flag automatically removes the container after it exits.
 
 # -v "$(pwd)":/workspace -> Mount the current working directory into the container. maps pwd to /workspace in the 
-# container
+# container 
+
+# -v /var/run/docker.sock:/var/run/docker.sock  -> binds the docker daemon to the container (removes warning)
 
 # -w /workspace -> Set the working directory inside the container to /workspace.
 
